dced648854887cfff69b5e239fcfceba3e6a5f37
Sumit.Verma <sumit@oscprofessionals.com>
2014-06-14 18:15:26 +0530

    change made for removing getModels from template file and create method in helper.

--- app/code/local/Oscp/AddCustomerAttribute/Helper/Data.php
+++ app/code/local/Oscp/AddCustomerAttribute/Helper/Data.php
@@ -1,5 +1,13 @@
 <?php
 class Oscp_AddCustomerAttribute_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+	//Function to get retailer store name.
+    public function getRetailerStoreName($retailer)
+    {
+		$retailerId = Mage::getModel('catalog/product')->load($retailer->getProduct()->getId())->getSellerId();
+		$retailerStoreName = $retailer->getLayout()->getBlockSingleton('Apptha_Marketplace_Block_Linkseller')->sellerdisplay($retailerId);
+		return $retailerStoreName;
+    }
 }
 	 
