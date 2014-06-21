3a6c6bbe82d5cfa2d29f5265635ab1d4b8db8cd3
Sumit.Verma <sumit@oscprofessionals.com>
2014-06-21 19:53:59 +0530

    changes to add seller's name and link on product detail, cart, home page, listing page.

--- app/code/local/Oscp/AddCustomerAttribute/Helper/Data.php
+++ app/code/local/Oscp/AddCustomerAttribute/Helper/Data.php
@@ -1,13 +1,29 @@
 <?php
+/**
+ *
+ * @package    Oscp_AddCustomerAttribute
+ * @author     2jDesign Team
+ * @Date       21/06/2014
+ *
+ * */
 class Oscp_AddCustomerAttribute_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
-	//Function to get retailer store name.
-    public function getRetailerStoreName($retailer)
+	public function getRetailerStoreName($product_id)
     {
-		$retailerId = Mage::getModel('catalog/product')->load($retailer->getProduct()->getId())->getSellerId();
-		$retailerStoreName = $retailer->getLayout()->getBlockSingleton('Apptha_Marketplace_Block_Linkseller')->sellerdisplay($retailerId);
+		$retailerId = Mage::getSingleton('catalog/product')->load($product_id)->getSellerId();
+		$retailerStoreName = Mage::getBlockSingleton('Apptha_Marketplace_Block_Linkseller')->sellerdisplay($retailerId);
 		return $retailerStoreName;
     }
+	public function getRetailerStoreLink($product_id)
+    {
+		$retailerId = Mage::getSingleton('catalog/product')->load($product_id)->getSellerId();
+		$show_profile = Mage::getBlockSingleton('Apptha_Marketplace_Block_Linkseller')->sellerprofiledisplay($retailerId);
+        if ($show_profile == 1) {
+			$target_path = 'marketplace/seller/displayseller/id/' . $retailerId;
+			$mainUrlRewrite     = Mage::getModel('core/url_rewrite')->load($target_path, 'target_path');
+			$getRequestPath     = $mainUrlRewrite->getRequestPath();
+			$get_requestPath    = Mage::getUrl($getRequestPath);
+			return $get_requestPath;
+		}
+    }
 }
-	 
